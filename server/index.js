10e3e77bbd0adbe04c48f1cf2de71308f27b6ff4
Igor Smyrnov <smyrnovai@gmail.com>
2017-02-25 15:23:38 +0100

    Moved all config data to config.json

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 "use strict";
 
+let config = require('./config.json');
 let express = require('express');
 let mysql = require('mysql');
 let app = express();
 
+
 let pool =  mysql.createPool({
-    host : 'localhost',
-    user : 'root',
-    password: '',
-    database: 'shops-online'
+    host: config.host,
+    user: config.db_user,
+    password: config.db_password,
+    database: config.db_name
 });
 
 let selectAllProducts = 'SELECT * FROM product';
@@ -16,6 +18,7 @@ let selectSingleProductBySlug = 'SELECT * FROM product WHERE slug = ?';
 
 app.get('/getProducts', function (request, response) {
     pool.getConnection(function (err, connection) {
+        if(err) throw err;
 
         //selectAllProducts
         connection.query(selectAllProducts, function (err, rows) {
@@ -34,10 +37,8 @@ app.get('/getProductBySlug/:slug', function (request, response) {
         //selectAllProducts
         connection.query(selectSingleProductBySlug, [request.params.slug], function (err, rows) {
             if(err) throw err;
-            else {
-                console.log(request.params);
+            else
                 response.send(rows[0]);
-            }
         });
 
         connection.release();
@@ -46,9 +47,9 @@ app.get('/getProductBySlug/:slug', function (request, response) {
 
 // Server starter
 
-app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname + "../../"));
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || config.port);
 app.listen(app.get('port'), function () {
     console.log('App started at localhost:' + app.get('port'));
 });
