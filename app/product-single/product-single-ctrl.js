07f439d463ca851ddbe8fba5b5b6b2239a348dd3
Igor Smyrnov <smyrnovai@gmail.com>
2017-03-22 04:33:45 +0100

    Angular routing improvement (forgotten fragment) + menu style

--- app/product-single/product-single-ctrl.js
+++ app/product-single/product-single-ctrl.js
@@ -3,7 +3,7 @@
 SO.controller('productSingleController',
     [ '$scope', '$state', 'dataFactory',
         function ($scope, $state, dataFactory) {
-            dataFactory.getProductBySlug($state.params.slug)
+            dataFactory.getProductById($state.params.id)
                 .then(function (data) {
                     $scope.product = data;
                     dataFactory.getShopById($scope.product.shop_id)
